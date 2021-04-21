2fdf6f5bbe9b91058defa1240b32a9775d4981c2
Jin Heo <993jin@gmail.com>
2021-04-20 21:22:44 -0400

    Add opencv and opengl to the build system

---
+++ cmake/opencv.cmake
@@ -0,0 +1,15 @@
+macro(use_opencv)
+  pkg_search_module(OPENCV REQUIRED opencv)
+  if(OPENCV_FOUND)
+    message(STATUS "[OpenCV Details]")
+    message("\t OPENCV_INCLUDE_DIRS: ${OPENCV_INCLUDE_DIRS}")
+    message("\t OPENCV_LDFLAGS: ${OPENCV_LDFLAGS}")
+
+    include_directories(${OPENCV_INCLUDE_DIRS})
+    list(APPEND PCC_CXX_FLAGS ${OPENCV_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${OPENCV_LDFLAGS})
+  endif(OPENCV_FOUND)
+endmacro()
+
+use_opencv()
+
