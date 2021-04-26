81688a86f8676c0a2eaf2221577e53a4fba11075
Jin Heo <993jin@gmail.com>
2021-04-26 19:40:50 +0000

    update cmake scripts for opengl

--- cmake/opengl.cmake
+++ cmake/opengl.cmake
@@ -1,23 +1,34 @@
 macro(use_opengl)
-  #pkg_search_module(OGL REQUIRED gl)
-  #if(OGL_FOUND)
-  #  message(STATUS "[OpenGL Details]")
-  #  message("\t OPENGL_INCLUDE_DIRS: ${OGL_INCLUDE_DIRS}")
-  #  message("\t OPENGL_LDFLAGS: ${OGL_LDFLAGS}")
-  #  list(APPEND 3D_PCC_CXX_FLAGS ${OGL_CFLAGS_OTHER})
-  #  list(APPEND 3D_PCC_LINKER_FLAGS ${OGL_LDFLAGS})
-  #endif()
+  pkg_search_module(GLFW REQUIRED glfw3 glfw)
+  if(GLFW_FOUND)
+    message(STATUS "[GLFW Details]")
+    message("\t GLFW_INCLUDE_DIRS: ${GLFW_INCLUDE_DIRS}")
+    message("\t GLFW_LDFLAGS: ${GLFW_LDFLAGS}")
+
+    list(APPEND PCC_CXX_FLAGS ${GLFW_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${GLFW_LDFLAGS})
+  endif()
 
 
-  #pkg_search_module(EGL REQUIRED egl)
-  #if(EGL_FOUND)
-  #  message(STATUS "[EGL Details]")
-  #  message("\t EGL_INCLUDE_DIRS: ${EGL_INCLUDE_DIRS}")
-  #  message("\t EGL_LDFLAGS: ${EGL_LDFLAGS}")
-  #
-  #  list(APPEND 3D_PCC_CXX_FLAGS ${EGL_CFLAGS_OTHER})
-  #  list(APPEND 3D_PCC_LINKER_FLAGS ${EGL_LDFLAGS})
-  #endif()
+  pkg_search_module(OGL REQUIRED gl)
+  if(OGL_FOUND)
+    message(STATUS "[OpenGL Details]")
+    message("\t OPENGL_INCLUDE_DIRS: ${OGL_INCLUDE_DIRS}")
+    message("\t OPENGL_LDFLAGS: ${OGL_LDFLAGS}")
+    list(APPEND PCC_CXX_FLAGS ${OGL_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${OGL_LDFLAGS})
+  endif()
+
+
+  pkg_search_module(EGL REQUIRED egl)
+  if(EGL_FOUND)
+    message(STATUS "[EGL Details]")
+    message("\t EGL_INCLUDE_DIRS: ${EGL_INCLUDE_DIRS}")
+    message("\t EGL_LDFLAGS: ${EGL_LDFLAGS}")
+
+    list(APPEND PCC_CXX_FLAGS ${EGL_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${EGL_LDFLAGS})
+  endif()
 
 
   pkg_search_module(GLEW REQUIRED glew)
@@ -28,21 +39,9 @@ macro(use_opengl)
 
     #message("\t GLEW_LIBRARIES: ${GLEW_LIBRARIES}")
     #list(APPEND 3D_PCC_LINKER_LIBS ${GLEW_LIBRARIES})
-    list(APPEND 3D_PCC_CXX_FLAGS ${GLEW_CFLAGS_OTHER})
-    list(APPEND 3D_PCC_LINKER_FLAGS ${GLEW_LDFLAGS})
+    list(APPEND PCC_CXX_FLAGS ${GLEW_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${GLEW_LDFLAGS};-lrt;-lm;-ldl;-lX11;-lpthread;-lxcb;-lXau;-lXdmcp)
   endif()
-
-
-  pkg_search_module(GLFW REQUIRED glfw3 glfw)
-  if(GLFW_FOUND)
-    message(STATUS "[GLFW Details]")
-    message("\t GLFW_INCLUDE_DIRS: ${GLFW_INCLUDE_DIRS}")
-    message("\t GLFW_LDFLAGS: ${GLFW_LDFLAGS}")
-
-    list(APPEND 3D_PCC_CXX_FLAGS ${GLFW_CFLAGS_OTHER})
-    list(APPEND 3D_PCC_LINKER_FLAGS ${GLFW_LDFLAGS})
-  endif()
-
 
 endmacro()
 
