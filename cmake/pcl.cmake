8b928b3581e361aecb3382e973a6d35de8d23908
Jin Heo <993jin@gmail.com>
2021-07-19 12:16:55 -0400

    feat: add PCL as dependency

---
+++ cmake/pcl.cmake
@@ -0,0 +1,19 @@
+macro(use_pcl)
+  find_package(PCL 1.3 REQUIRED)
+  if(PCL_FOUND)
+    message(STATUS "[PCL Details]")
+    message("\t PCL_INCLUDE_DIRS: ${PCL_INCLUDE_DIRS}")
+    message("\t PCL_LDFLAGS: ${PCL_LDFLAGS}")
+    message("\t PCL_LIBRARIES: ${PCL_LIBRARIES}")
+    message("\t PCL_LIBRARY_DIRS: ${PCL_LIBRARY_DIRS}")
+    message("\t PCL_DEFINITIONS: ${PCL_DEFINITIONS}")
+
+    include_directories(${PCL_INCLUDE_DIRS})
+    add_definitions(${PCL_DEFINITIONS})
+    list(APPEND PCC_CXX_FLAGS ${PCL_CFLAGS_OTHER})
+    list(APPEND PCC_LINKER_FLAGS ${PCL_LIBRARIES})
+  endif(PCL_FOUND)
+endmacro()
+
+use_pcl()
+
