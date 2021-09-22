3b1ad34e844e3baf0ce58e66560ba618fce0c41f
Jin Heo <993jin@gmail.com>
2021-09-22 00:53:46 -0400

    test: more opt flow

--- source/pc_predictor.cc
+++ source/pc_predictor.cc
@@ -10,10 +10,10 @@ PclPcXYZ PcPredictor::predictNextPc(PclPcXYZ origPc, VecXyz velocity)
   for(auto oP: origPc->points)
   {
     pcl::PointXYZ pP;
-    pP.x = oP.x - (velocity.x/KITTI_DATASET_FREQUENCY);
-    pP.y = oP.y - (velocity.y/KITTI_DATASET_FREQUENCY);
-    //pP.z = oP.z - (velocity.z/KITTI_DATASET_FREQUENCY);
-    pP.z = oP.z;
+    pP.x = oP.x + (velocity.x/KITTI_DATASET_FREQUENCY);
+    pP.y = oP.y + (velocity.y/KITTI_DATASET_FREQUENCY);
+    pP.z = oP.z + (velocity.z/KITTI_DATASET_FREQUENCY);
+    //pP.z = oP.z;
 
     predPc->push_back(pP);
   }
