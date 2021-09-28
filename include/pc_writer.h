fda8342aa304bf958530f64e0a0bff825c01f9e2
Jin Heo <993jin@gmail.com>
2021-09-27 21:49:31 -0400

    feat: add pc_writer

---
+++ include/pc_writer.h
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <bag_reader.h>
+#include <types.h>
+#include <pcl/common/common_headers.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/ply_io.h>
+#include <defs.h>
+
+#ifndef __PCC_PCWRITER__
+#define __PCC_PCWRITER__
+
+class PcWriter
+{
+  public:
+    PcWriter();
+    void writeBin(std::string fileName, PclPcXYZ pc);
+    void writeBin(std::string fileName, PclPcXYZI pc);
+    void writePcd(std::string fileName, PclPcXYZ pc, bool compress);
+    void writePcd(std::string fileName, PclPcXYZI pc, bool compress);
+    void writePly(std::string fileName, PclPcXYZ pc);
+    void writePly(std::string fileName, PclPcXYZI pc);
+};
+
+#endif
+
---
+++ source/pc_writer.cc
@@ -0,0 +1,76 @@
+#include "pcl/io/pcd_io.h"
+#include <pc_writer.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <utils.h>
+
+
+PcWriter::PcWriter()
+{
+}
+
+
+void PcWriter::writeBin(std::string fileName, PclPcXYZ pc)
+{
+  std::ofstream outStream(fileName, std::ofstream::binary);
+
+  float x, y, z;
+  for (auto p : pc->points) {
+    x = p.x;
+    y = p.y;
+    z = p.z;
+    outStream.write(reinterpret_cast<const char*>(&x), sizeof(x));
+    outStream.write(reinterpret_cast<const char*>(&y), sizeof(y));
+    outStream.write(reinterpret_cast<const char*>(&z), sizeof(z));
+  }
+  outStream.close();
+}
+
+
+void PcWriter::writeBin(std::string fileName, PclPcXYZI pc)
+{
+  std::ofstream outStream(fileName, std::ofstream::binary);
+
+  float x, y, z, i;
+  for (auto p : pc->points) {
+    x = p.x;
+    y = p.y;
+    z = p.z;
+    i = p.intensity;
+    outStream.write(reinterpret_cast<const char*>(&x), sizeof(x));
+    outStream.write(reinterpret_cast<const char*>(&y), sizeof(y));
+    outStream.write(reinterpret_cast<const char*>(&z), sizeof(z));
+    outStream.write(reinterpret_cast<const char*>(&i), sizeof(i));
+  }
+  outStream.close();
+}
+
+
+void PcWriter::writePcd(std::string fileName, PclPcXYZ pc, bool compress)
+{
+  if(compress)
+    pcl::io::savePCDFileBinaryCompressed(fileName, *pc);
+  else
+    pcl::io::savePCDFileBinary(fileName, *pc);
+}
+
+
+void PcWriter::writePcd(std::string fileName, PclPcXYZI pc, bool compress)
+{
+  if(compress)
+    pcl::io::savePCDFileBinaryCompressed(fileName, *pc);
+  else
+    pcl::io::savePCDFileBinary(fileName, *pc);
+}
+
+
+void PcWriter::writePly(std::string fileName, PclPcXYZ pc)
+{
+  pcl::io::savePLYFile(fileName, *pc);
+}
+
+
+void PcWriter::writePly(std::string fileName, PclPcXYZI pc)
+{
+  pcl::io::savePLYFile(fileName, *pc);
+}
+
