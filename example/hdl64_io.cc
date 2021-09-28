7eb6bab9f921e3a6fe19a7f8b29c1fbac60abab1
Jin Heo <993jin@gmail.com>
2021-09-27 21:52:27 -0400

    feat: add hdl64_io example

---
+++ example/hdl64_io.cc
@@ -0,0 +1,42 @@
+#include <3dpcc>
+
+using namespace std;
+
+int main() {
+  double st, et;
+
+  PcReader pcReader;
+  HDL64RIConverter hdl64RiConverter;
+
+  PcWriter pcWriter;
+
+
+  PclPcXYZI pc;
+
+  st = getTsNow();
+  pc = pcReader.readXyziBin("0000000000.bin");
+
+  debug_print("points: %d", pc->points.size());
+  et = getTsNow();
+  debug_print("readT: %f ms", et-st);
+  pcWriter.writeBin("pc.bin", pc);
+  pcWriter.writePcd("pc_ncomp.pcd", pc, false);
+  pcWriter.writePcd("pc_comp.pcd", pc, true);
+  pcWriter.writePly("pc.ply", pc);
+
+  PclPcXYZI pc2;
+  pc2 = pcReader.readXyziBin("pc.bin");
+  debug_print("points: %d", pc2->points.size());
+  pc2 = pcReader.readXyziPcd("pc_comp.pcd");
+  debug_print("points: %d", pc2->points.size());
+  pc2 = pcReader.readXyziPcd("pc_ncomp.pcd");
+  debug_print("points: %d", pc2->points.size());
+  pc2 = pcReader.readXyziPly("pc.ply");
+  debug_print("points: %d", pc2->points.size());
+
+  Visualizer pcVisualizer;
+  pcVisualizer.initViewerXYZ();
+  pcVisualizer.setViewer(pc2);
+  pcVisualizer.show(3000);
+}
+
