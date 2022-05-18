b7473c81addf22c0710c224439654894d78a454a
Jin Heo <993jin@gmail.com>
2022-05-18 18:33:56 -0400

    refactor: update pc_writer

--- include/pc_writer.h
+++ include/pc_writer.h
@@ -1,31 +1,30 @@
+#pragma once
+
 #include <bits/stdc++.h>
-#include <bag_reader.h>
-#include <types.h>
 #include <pcl/common/common_headers.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/surface/organized_fast_mesh.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/io/obj_io.h>
+
+#include <types.h>
 #include <defs.h>
 
-#ifndef __PCC_PCWRITER__
-#define __PCC_PCWRITER__
-
+namespace flicr
+{
 class PcWriter
 {
   public:
-    PcWriter();
-    void writeBin(std::string fileName, PclPcXYZ pc);
-    void writeBin(std::string fileName, PclPcXYZI pc);
+    void writeBin(std::string fileName, types::PclPcXyz pc);
+    void writeBin(std::string fileName, types::PclPcXyzi pc);
 
-    void writePcd(std::string fileName, PclPcXYZ pc, bool compress);
-    void writePcd(std::string fileName, PclPcXYZI pc, bool compress);
+    void writePcd(std::string fileName, types::PclPcXyz pc, bool compress);
+    void writePcd(std::string fileName, types::PclPcXyzi pc, bool compress);
 
-    void writePly(std::string fileName, PclPcXYZ pc);
-    void writePly(std::string fileName, PclPcXYZI pc);
+    void writePly(std::string fileName, types::PclPcXyz pc);
+    void writePly(std::string fileName, types::PclPcXyzi pc);
 
     void writePlyFromMesh(std::string fileName, pcl::PolygonMeshPtr mesh);
 };
-
-#endif
+}
 
