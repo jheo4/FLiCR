a69be162f827e3d1848ee59d23af42b2f2a7bd0c
Jin Heo <993jin@gmail.com>
2022-05-18 18:32:30 -0400

    refactor: update pc reader

--- include/pc_reader.h
+++ include/pc_reader.h
@@ -1,39 +1,39 @@
+#pragma once
+
 #include <bits/stdc++.h>
-#include <bag_reader.h>
-#include <types.h>
 #include <pcl/common/common_headers.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/surface/gp3.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/search/kdtree.h>
 #include <pcl/io/ply_io.h>
+
+#include <types.h>
 #include <defs.h>
 
-#ifndef __PCC_PCREADER__
-#define __PCC_PCREADER__
-
+namespace flicr
+{
 class PcReader
 {
   public:
     PcReader();
-    RawPc rawReadXyzBin(std::string fileName);
+    types::RawPc rawReadXyzBin(std::string fileName);
 
-    PclPcXYZ  readXyzBin (std::string fileName);
-    PclPcXYZ  readXyzFromXyziBin (std::string fileName);
-    PclPcXYZI readXyziBin(std::string fileName);
+    types::PclPcXyz  readXyzBin (std::string fileName);
+    types::PclPcXyz  readXyzFromXyziBin (std::string fileName);
+    types::PclPcXyzi readXyziBin(std::string fileName);
 
-    bool readXyzInt(std::string fileName, PclPcXYZ &pc, std::vector<float> &intensity);
+    bool readXyzInt(std::string fileName, types::PclPcXyz &pc, std::vector<float> &intensity);
 
-    PclPcXYZ  readXyzPcd (std::string fileName);
-    PclPcXYZI readXyziPcd(std::string fileName);
+    types::PclPcXyz  readXyzPcd (std::string fileName);
+    types::PclPcXyzi readXyziPcd(std::string fileName);
 
-    PclPcXYZ  readXyzPly (std::string fileName);
-    PclPcXYZI readXyziPly(std::string fileName);
-    PclMesh   readMeshPly(std::string fileName);
+    types::PclPcXyz  readXyzPly (std::string fileName);
+    types::PclPcXyzi readXyziPly(std::string fileName);
+    types::PclMesh   readMeshPly(std::string fileName);
 
-    void generateMeshFromXyz (PclPcXYZ pc, pcl::PolygonMeshPtr &mesh,
+    void generateMeshFromXyz (types::PclPcXyz pc, pcl::PolygonMeshPtr &mesh,
                               std::vector<int> partID, std::vector<int> pointStates);
 };
-
-#endif
+}
 
