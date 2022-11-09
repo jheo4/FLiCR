768db02c3f132a6103bb73898b217b130824feae
Jin Heo <993jin@gmail.com>
2022-11-09 02:59:22 +0000

    fix: resolve non-overwrite error by rm

--- scripts/interpolations.sh
+++ scripts/interpolations.sh
@@ -1,3 +1,7 @@
+rm *.bin
+rm *.png
+rm *.yaml
+
 # normalized ri/intmap
 ../bin/ri_intmap_downsampler -i ../sample_data/0.bin -o out_ri.png -m out_int.png --yaw_fov 360 --pitch_fov 26.8 --yaw_offset 180 --pitch_offset -88 --min_range 1 --max_range 80 -x 256 -y 64 -n 1 -d 1
 # ri/intmap as float values
