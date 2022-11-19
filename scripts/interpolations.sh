d9c990e1878c756a1968bfa3f47d239bed1ed3f6
Jin Heo <993jin@gmail.com>
2022-11-19 16:18:12 +0000

    feat: update scripts with added examples

--- scripts/interpolations.sh
+++ scripts/interpolations.sh
@@ -2,6 +2,9 @@ rm *.bin
 rm *.png
 rm *.yaml
 
+../bin/pc_downsample_interpolator -i ../sample_data/0.bin -o interpolated_pc.bin --yaw_fov 360 --pitch_fov 26.8 --yaw_offset 180 --pitch_offset -88 --min_range 1 --max_range 80 -x 256 -y 64 --wnd_size 4 --intr_per_wnd 2 --circular 1 --grad_thresh 2 --policy random -d 1
+../bin/pc_downsampler_cv_interpolator -i ../sample_data/0.bin -o interpolated_pc.bin --yaw_fov 360 --pitch_fov 26.8 --yaw_offset 180 --pitch_offset -88 --min_range 1 --max_range 80 -x 256 -y 64  -a linear  --intr_x 320 --intr_y 64 -d 1
+
 # normalized ri/intmap
 ../bin/ri_intmap_downsampler -i ../sample_data/0.bin -o out_ri.png -m out_int.png --yaw_fov 360 --pitch_fov 26.8 --yaw_offset 180 --pitch_offset -88 --min_range 1 --max_range 80 -x 256 -y 64 -n 1 -d 1
 # ri/intmap as float values
