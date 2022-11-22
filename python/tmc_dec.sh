1a6450bce1e640c101c06a2b95d35095a0f64e13
Jin Heo <993jin@gmail.com>
2022-11-22 11:24:15 -0500

    exp: bkup

---
+++ python/tmc_dec.sh
@@ -0,0 +1,9 @@
+for iter in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+do
+  ./build/tmc3/tmc3 --mode=1 \
+  --compressedStreamPath=/home/jin/mnt/github/PCC_Comp/mpeg-pcc-tmc13/test/test.bin \
+  --reconstructedDataPath=/home/jin/mnt/github/PCC_Comp/mpeg-pcc-tmc13/test/reconstructed.ply \
+  --outputUnitLength=0 \
+  --outputBinaryPly=0
+done
+
---
+++ python/tmc_enc.sh
@@ -0,0 +1,13 @@
+for iter in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+do
+  ./build/tmc3/tmc3 --mode=0 \
+  --uncompressedDataPath=/home/jin/mnt/Data/kitti/tmc3_original_xyz.ply \
+  --compressedStreamPath=/home/jin/mnt/github/PCC_Comp/mpeg-pcc-tmc13/test/test.bin \
+  --outputUnitLength=1 \
+  --outputScaling=0 \
+  --outputPrecisionBits=-1 \
+  --srcUnitLength=1 \
+  --srcUnit=1 \
+  --inputScale=100
+done
+
